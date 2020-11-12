3b423b65c5feede8ee7a3440ce8426592eb25988
LieveOtten <71327022+LieveOtten@users.noreply.github.com>
2020-11-12 09:36:34 +0100

    Add files via upload

---
+++ js/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(windowWidth -10,windowHeight,-10);
+  background ("RED");
+
+  line (width/2,0,width/2,height);
+  line (width/4,0,width/4,height);
+  line (width/4*3,0,width/4*3,height);
+  line(0,0,0,height);
+  line(width -1,width,height);
+ 
+  line(0,height/3,width,height/3);
+  line (0,height/3*2,width,height/3*2);
+
+  fill(0,255,0);
+  rect(width/4,height/3,widht/4,height/4);
+  
+}
+
+function draw() {
+  
+  
+}
